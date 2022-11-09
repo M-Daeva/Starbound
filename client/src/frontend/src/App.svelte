768760f6852463f2f78659b4d340bf2c2732cea3
M. Daeva <embedmad@gmail.com>
2022-11-09 23:27:01 +0300

    added table on assets page

--- client/src/frontend/src/App.svelte
+++ client/src/frontend/src/App.svelte
@@ -14,35 +14,37 @@
 </script>
 
 <Router>
-  <div class="container">
+  <div class="container bg-indigo-900 text-amber-200">
     <nav>
-      <ul>
-        <li class="wrapper">
-          <Link class="my-link" to={paths.dashboard}>Dashboard</Link>
+      <ul class="my-5">
+        <li class="wrapper bg-amber-200">
+          <Link
+            class="my-link btn btn-primary bg-amber-200"
+            to={paths.dashboard}>Dashboard</Link
+          >
         </li>
-        <li class="wrapper">
-          <Link class="my-link" to={paths.assets}>Assets</Link>
+        <li class="wrapper bg-amber-200">
+          <Link class="my-link btn btn-primary bg-amber-200" to={paths.assets}
+            >Assets</Link
+          >
         </li>
-        <li class="wrapper">
-          <Link class="my-link" to={paths.bank}>Bank</Link>
+        <li class="wrapper bg-amber-200">
+          <Link class="my-link btn btn-primary bg-amber-200" to={paths.bank}
+            >Bank</Link
+          >
         </li>
       </ul>
     </nav>
 
     <div>
       <Route primary={false} path={paths.dashboard}><Dashboard /></Route>
-      <Route path={paths.assets}><Assets /></Route>
+      <Route primary={false} path={paths.assets}><Assets /></Route>
       <Route path={paths.bank}><Bank /></Route>
     </div>
   </div>
 </Router>
 
 <style>
-  .container {
-    background-color: rgb(45, 2, 47);
-    color: rgb(245, 236, 152);
-  }
-
   nav {
     display: flex;
     justify-content: center;
@@ -57,7 +59,6 @@
   }
 
   li {
-    background-color: rgb(245, 236, 152);
     display: inline-block;
     padding: 5px;
     width: 100px;
@@ -66,7 +67,7 @@
   }
 
   .wrapper :global(.my-link) {
-    color: rgb(45, 2, 47);
+    color: rgb(49, 46, 129);
   }
 
   .wrapper :global(.my-link):hover {
--- client/src/frontend/src/routes/Assets.svelte
+++ client/src/frontend/src/routes/Assets.svelte
@@ -1,1 +1,49 @@
-<div><h1>Assets</h1></div>
+<script lang="ts">
+  interface Row {
+    logo: string;
+    asset: string;
+    address: string;
+    ratio: string;
+    validator: string;
+    isGranted: boolean;
+  }
+
+  let row = {
+    logo: "https://raw.githubusercontent.com/cosmos/chain-registry/68df154360a341831f557ee30119dbbec1a77ca8/osmosis/images/osmo.svg",
+    asset: "OSMO",
+    address: "osmo1gjqnuhv52pd2a7ets2vhw9w9qa9knyhy7y9tgx",
+    ratio: "0.2",
+    validator: "Imperator",
+    isGranted: false,
+  };
+
+  let rows: Row[] = [...new Array(35)].map((_) => row);
+</script>
+
+<div class="table-container">
+  <div class="overflow-x-auto">
+    <table class="table table-compact w-full">
+      <thead>
+        <tr>
+          {#each Object.keys(row).map((item) => item[0].toUpperCase() + item.slice(1)) as key}
+            <th>{key}</th>
+          {/each}
+        </tr>
+      </thead>
+
+      <tbody>
+        {#each rows as { logo, asset, address, ratio, validator, isGranted }}
+          <tr>
+            <td class="w-1/12"><img class="w-1/2" src={logo} alt="logo" /></td>
+            <td>{asset}</td>
+            <td><input type="text" value={address} /></td>
+            <td><input type="number" value={ratio} /></td>
+            <td>{validator}</td>
+            <td><button>{isGranted ? "Revoke" : "Grant"}</button></td>
+            <td><button>❌</button></td>
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  </div>
+</div>
